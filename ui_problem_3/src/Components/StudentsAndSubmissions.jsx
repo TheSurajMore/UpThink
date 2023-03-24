b8cfac80cc3df66fd80f1f328102ee733326f81f
TheSurajMore <surajmbusiness@gmail.com>
2023-03-24 08:20:39 +0530

    Update

---
+++ ui_problem_3/src/Components/StudentsAndSubmissions.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/students_and_submissions.css"
+
+const StudentsAndSubmissions = () =>{
+    return(
+        <div className="students_and_submissions" >
+            <div>
+                <h1>2</h1>
+                ACTIVE  STUDENTS
+            </div>
+            <div>
+                <h1>2</h1>
+                SUBMISSIONS
+            </div>
+        </div>
+    )
+}
+
+export default StudentsAndSubmissions;
