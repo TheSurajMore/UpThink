bfeffdc865eaa1e27b7a0f80fb8d591087f03538
TheSurajMore <surajmbusiness@gmail.com>
2023-03-25 10:25:25 +0530

    Update

--- suraj_more/src/App.js
+++ suraj_more/src/App.js
@@ -5,24 +5,29 @@ function App() {
     <div className="App">
       <h1>Suraj More</h1>
       <div>
-        UI Problem 1 
+        UI Problem 1 (100% Completed)
         <a href='https://surajmoreuiproblem1.netlify.app' target="_blank" rel="noreferrer" > <button>Solution</button> </a>
         <a href='https://github.com/TheSurajMore/UpThink/tree/main/ui_problem_1' target="_blank" rel="noreferrer" > <button>Code</button> </a> 
       </div>
       <div>
-        UI Problem 3 
+        UI Problem 3 (100% Completed)
         <a href='https://surajmoreuiproblem3.netlify.app' target="_blank" rel="noreferrer"> <button>Solution</button> </a> 
         <a href='https://github.com/TheSurajMore/UpThink/tree/main/ui_problem_3' target="_blank" rel="noreferrer"> <button>Code</button></a> 
       </div>
       <div>
-        UI Problem 5 
+        UI Problem 5 (100% Completed)
         <a href='https://surajmoreuiproblem5.netlify.app' target="_blank" rel="noreferrer"> <button>Solution</button> </a> 
         <a href='https://github.com/TheSurajMore/UpThink/tree/main/ui_problem_5' target="_blank" rel="noreferrer"> <button>Code</button> </a>
       </div>
       <div>
-        UI Problem 6 
+        UI Problem 6 (100% Completed)
         <a href='https://surajmoreuiproblem6.netlify.app' target="_blank" rel="noreferrer"> <button>Solution</button> </a> 
         <a href='https://github.com/TheSurajMore/UpThink/tree/main/ui_problem_6' target="_blank" rel="noreferrer"> <button>Code</button> </a>
+      </div>
+      <div>
+        UI Problem 2 (50% Completed)
+        <a href='https://surajmoreuiproblem2.netlify.app' target="_blank" rel="noreferrer"> <button>Solution</button> </a> 
+        <a href='https://github.com/TheSurajMore/UpThink/tree/main/ui_problem_2' target="_blank" rel="noreferrer"> <button>Code</button> </a>
       </div>
     </div>
   );
