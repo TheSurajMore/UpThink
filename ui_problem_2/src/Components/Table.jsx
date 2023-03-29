c741d9fdd80233b5af3ea118dbc92f1767265e68
TheSurajMore <surajmbusiness@gmail.com>
2023-03-29 14:24:13 +0530

    Update

--- ui_problem_2/src/Components/Table.jsx
+++ ui_problem_2/src/Components/Table.jsx
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../data.json";
 import "../styles/table.css"
+import DialogueBox from "./DialogueBox";
 
 const Table = () => {
+
+  const [show, setShow] = useState(false)
+  const [id, setId] = useState('')
+
+ const handleClick = (el) => {
+  setId(el)
+  setShow(true)
+ }
 
     return(
         <div className="table" >
             <h2>Manage Users</h2>
             <table>
-                <thead>
+                <tr>
                   <th>Name</th>
                   <th>Email</th>
                   <th>Groups</th>
-                </thead>
+                </tr>
                 {
                 data.map((el)=>(
-                  <tbody key={el.id} >
+                  <tr key={el.id} onClick={()=>handleClick(el)} >
                     <td>{el.name}</td>
                     <td>{el.email}</td>
                     <td><button style={{backgroundColor:el.role==="Admin"?"red":el.role==="Instructor"?"violet":"blue", color:"white", border:"none" }} >
                         {el.role}</button></td>
-                  </tbody>
+                  </tr>
                 ))
                 }
               </table>
-            
+              {
+                show && <DialogueBox name={id.name} email={id.email} now={id.role} 
+                //Admin
+                change1={id.role===Admin?"Student":''}
+                change2={id.role===Admin?"Instructor":''} />
+              }
         </div>
     )
 }
